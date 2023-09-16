92a2f584574f3c1efd1eb086b3e8782c90a75182
Nate Sobeck <nsobeck910@gmail.com>
2023-09-16 17:17:33 -0400

    add index functionality and view

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,6 +6,34 @@ function newFlight(req, res) {
   })
 }
 
+function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') {
+      delete req.body[key]
+    }
+  }
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights'
+    })
+  })
+}
+
 export {
   newFlight as new,
+  create,
+  index,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,8 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 // GET localhost:3000/users
+router.get('/', flightsCtrl.index)
 router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
 
 export { router }
